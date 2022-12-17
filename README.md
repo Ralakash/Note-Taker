325ca85744eb763355999be9ace96b4535d25b81
Ralakash <shawnkaralash@gmail.com>
2022-12-17 10:28:37 -0500

    adds comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Note-Taker
+# Note Taker
+
+![License](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Desription
+
+Note Taker is a site that allows you to create notes and send them to be sent on a server where they are stored remotely. It allows for the notes to be called from that server and to be deleted .
+This project was used to demonstrate the ability to use get and push methods to allow a server to access and delete data when directed from a website.
+
+During this project I learned how to use express.js more thuroughly and was able to better understand the functionality behind the get and push methods.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [Tests](#tests)
+- [License](#license)
+
+## Installation
+
+To install the project copy the files onto your computer. Open an terminal with the path set to start in the Note-Taker folder. Type NPM Install. After the instalation has occured type "node server.js" . This will start the server.
+
+## Usage
+
+Type notes into the website and they will be saved to the server.
+![Screenshot](./Assets/screenshot.png)
+
+## Credits
+
+Help given by BCS Tutoring Services.
+
+## Questions
+
+Please contact me with any questions at Ralakash
+or email me at shawnkaralash@gmail.com
+
+## License
+
+This project is licensed under MIT license
--- server.js
+++ server.js
@@ -1,25 +1,32 @@
+// Calls the packages needed to access express, path, the file system, and uniqid
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uniqid = require('uniqid');
+
+// adds the ability to call express through the variable app.
 const app = express();
 
+// gives us a port acccess that defaults to 3001 if no other port is given.
 const PORT = process.env.PORT || 3001;
 
+// allows our server to use JSON and to access the public folder.
 app.use(express.json());
 app.use(express.static('public'));
 
+// creates a get method that redirects you to the notes HTML page.
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+// creates a get function that reads the information in the db.json file and sends it back
 app.get('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf-8', (error, data) => {
     res.send(data);
   });
 });
 
-// Add notes
+// Add notes to the db.json file and sends back the information.
 app.post('/api/notes', (req, res) => {
   fs.readFile('./db/db.json', 'utf-8', (error, data) => {
     const notes = JSON.parse(data);
@@ -30,7 +37,7 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
-// Delete posts
+// creates a method to delete previously made notes based on their unique id.
 app.delete('/api/notes/:id', (req, res) => {
   fs.readFile('./db/db.json', 'utf-8', (error, data) => {
     const notes = JSON.parse(data);
@@ -41,10 +48,12 @@ app.delete('/api/notes/:id', (req, res) => {
   });
 });
 
+// creates a default get method that sends users to the home page.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+// console logs when the server goes live and what port it is running on.
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
